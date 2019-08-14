21a18b4b7e54046e277a2d9c12290e30cb350374
wygulmage <wygulmage@users.noreply.github.com>
2019-08-14 14:48:53 -0400

    changed Model to use infinite streams.

--- reactive-banana/src/Reactive/Banana/Model.hs
+++ reactive-banana/src/Reactive/Banana/Model.hs
@@ -1,170 +1,188 @@
-{-----------------------------------------------------------------------------
-    reactive-banana
-------------------------------------------------------------------------------}
-{-# LANGUAGE RecursiveDo #-}
-module Reactive.Banana.Model (
-    -- * Synopsis
-    -- | Model implementation for learning and testing.
+{-# LANGUAGE
+    RecursiveDo
+  #-}
 
-    -- * Overview
-    -- $overview
+module Reactive.Banana.Model where
 
-    -- * Core Combinators
-    -- ** Event and Behavior
-    Nat, Time,
-    Event(..), Behavior(..),
-    interpret,
-    -- ** First-order
-    module Control.Applicative,
-    never, unionWith, filterJust, apply,
-    -- ** Moment and accumulation
-    Moment(..), accumE, stepper,
-    -- ** Higher-order
-    valueB, observeE, switchE, switchB,
-    ) where
+import Prelude (($), (+), (-), const, flip, fst, snd, fromIntegral, replicate)
 
-import Control.Applicative
+import Control.Applicative (Applicative ((<*>), pure, liftA2))
+import Control.Category ((.), id)
 import Control.Monad
-import Control.Monad.Fix
+import Data.Function ((&))
+import Data.Bifunctor (bimap)
+import Data.Functor
+import Data.Semigroup
+import Data.Monoid
+import Data.Foldable (Foldable (foldr))
 
-{-$overview
+import Data.Functor.Compose (Compose (Compose), getCompose)
+import Data.Maybe
+import qualified Data.List as List
+import Numeric.Natural
 
-This module reimplements the key FRP types and functions from the module
-"Reactive.Banana.Combinators" in a way that is hopefully easier to understand.
-Thereby, this model also specifies the semantics of the library.
-Of course, the real implementation is much more efficient than this model here.
-
-To understand the model in detail, look at the source code!
-(If there is no link to the source code at every type signature,
-then you have to run cabal with --hyperlink-source flag.)
-
-This model is /authoritative/:
-Event functions that have been constructed using the same combinators
-/must/ give the same results when run with the @interpret@ function
-from either the module "Reactive.Banana.Combinators"
-or the module "Reactive.Banana.Model".
-This must also hold for recursive and partial definitions
-(at least in spirit, I'm not going to split hairs over @_|_@ vs @\\_ -> _|_@).
-
--}
-
-{-----------------------------------------------------------------------------
-    Event and Behavior
-------------------------------------------------------------------------------}
--- | Natural numbers (poorly represented).
-type Nat = Int
-
--- | The FRP model used in this library is actually a model with continuous time.
---
--- However, it can be shown that this model is observationally
--- equivalent to a particular model with (seemingly) discrete time steps,
--- which is implemented here.
--- The main reason for doing this is to be able to handle recursion correctly.
--- Details will be explained elsewhere.
-type Time = Nat -- begins at t = 0
-
--- | Event is modeled by an /infinite/ list of 'Maybe' values.
--- It is isomorphic to @Time -> Maybe a@.
---
--- 'Nothing' indicates that no occurrence happens,
--- while 'Just' indicates that an occurrence happens.
-newtype Event a = E { unE :: [Maybe a] } deriving (Show)
-
--- | Behavior is modeled by an /infinite/ list of values.
--- It is isomorphic to @Time -> a@.
-newtype Behavior a = B { unB :: [a] } deriving (Show)
 
 interpret :: (Event a -> Moment (Event b)) -> [Maybe a] -> [Maybe b]
-interpret f as =
-    take (length as) . unE . (\m -> unM m 0) . f . E $ (as ++ repeat Nothing)
+interpret f xs =
+  take (fromIntegral $ List.length xs) . unE . ($ 0) . f . Compose $ prependList xs (unE never)
 
-{-----------------------------------------------------------------------------
-    First-order
-------------------------------------------------------------------------------}
-instance Functor Event where
-    fmap f (E xs) = E (fmap (fmap f) xs)
+
+-- A behavior is modeled as an /infinite/ list of values.
+newtype Behavior a = Behavior{ getBehavior ::(a, Behavior a) }
+
+-- | Time is modeled as discrete and moving only forward.
+type Time = Max Natural
+
+-- | A moment gives a behavior at one point in time.
+-- (The isomorphism between Behavior and Moment is witnessed by (!!) and enumerate.)
+type Moment = (->) Time
+
+type Event = Compose Behavior Maybe
+
+----- Behavior Instances -----
 
 instance Functor Behavior where
-    fmap f (B xs) = B (fmap f xs)
+  fmap f = Behavior . bimap f (fmap f) . getBehavior
 
 instance Applicative Behavior where
-    pure x          = B $ repeat x
-    (B f) <*> (B x) = B $ zipWith ($) f x
+  pure x = xs where xs = cons x xs
+  liftA2 f = loop
+    where
+    loop xs ys =
+      cons (head xs `f` head ys) (tail xs `loop` tail ys)
+
+instance Semigroup a => Semigroup (Behavior a) where
+  -- There would be no point to putting behaviors end-to-end, so zip them.
+  (<>) = liftA2 (<>)
+
+instance Monoid a => Monoid (Behavior a) where
+  mempty = pure mempty
+  mappend = liftA2 mappend
+
+instance Foldable Behavior where
+  foldr f _ = foldr1 f
+
+
+----- Functions -----
+
+--- Behavior ---
+-- | Convert a Behavior to a function.
+(!!) :: Behavior a -> Time -> a
+xs !! n = (head . drop n) xs
+
+valueB :: Behavior a -> Time -> a
+valueB = (!!)
+
+time :: Behavior Time
+time = iterate (+ 1) 0
+
+-- | Sample each sub-behavior at its occurrence in the behavior.
+diagonalB :: Behavior (Behavior a) -> Behavior a
+diagonalB = liftA2 (flip (!!)) time
+
+-- | Create a Behavior from a function and a seed.
+iterate :: (a -> a) -> a -> Behavior a
+iterate f = loop
+   where
+   loop x = cons x (loop (f x))
+
+foldr1 :: (a -> b -> b) -> Behavior a -> b
+foldr1 f = loop
+   where
+   loop xs = head xs `f` loop (tail xs)
+
+prependList :: [a] -> Behavior a -> Behavior a
+prependList = flip (foldr cons)
+
+cons :: a -> Behavior a -> Behavior a
+cons x = Behavior . (,) x
+
+head :: Behavior a -> a
+head = fst . getBehavior
+
+tail :: Behavior a -> Behavior a
+tail = snd . getBehavior
+
+tails :: Behavior a -> Behavior (Behavior a)
+-- diagonalB . tails = id
+tails = iterate tail
+
+drop :: Time -> Behavior a -> Behavior a
+-- ^ Move t = 0 forward in time. (Or, relatively, move the behavior backward in time.)
+drop 0 = id
+drop n = drop (n - 1) . tail
+
+take :: Time -> Behavior a -> [a]
+-- ^ Sample a period of time starting at 0.
+take 0 _ = []
+take n (Behavior (x, xs)) = x : take (n - 1) xs
+
+cycle :: [a] -> Behavior a
+-- ^ Cycle through xs forever.
+cycle xs = xs' where xs' = prependList xs xs'
+
+
+--- Event ---
+
+unE :: Event a -> Behavior (Maybe a)
+unE = getCompose
 
 never :: Event a
-never = E $ repeat Nothing
+-- ^ Nothing ever happens.
+never = Compose (pure Nothing)
+
+timeE :: Event Time
+timeE = behaviorToEvent time
 
 unionWith :: (a -> a -> a) -> Event a -> Event a -> Event a
-unionWith f (E xs) (E ys) = E $ zipWith combine xs ys
-    where
-    combine (Just x) (Just y) = Just $ f x y
-    combine (Just x) Nothing  = Just x
-    combine Nothing  (Just y) = Just y
-    combine Nothing  Nothing  = Nothing
+unionWith f (Compose xs) (Compose ys) = Compose (liftA2 combine xs ys)
+   where
+   combine (Just x) = Just . maybe x (f x)
+   combine _ = id
 
 filterJust :: Event (Maybe a) -> Event a
-filterJust = E . fmap join . unE
+filterJust = Compose . fmap join . getCompose
 
-apply :: Behavior (a -> b) -> Event a -> Event b
-apply (B fs) = E . zipWith (\f mx -> fmap f mx) fs . unE
+forgetE :: Time -> Event a -> Event a
+-- ^ forget all events before time n. (I.e. move the events back in time.)
+forgetE n = Compose . drop n . getCompose
 
-{-----------------------------------------------------------------------------
-    Moment and accumulation
-------------------------------------------------------------------------------}
-newtype Moment a = M { unM :: Time -> a }
-
-instance Functor     Moment where fmap f = M . fmap f . unM
-instance Applicative Moment where
-    pure   = M . const
-    (<*>)  = ap
-instance Monad Moment where
-    return = pure
-    (M m) >>= k = M $ \time -> unM (k $ m time) time
-
-instance MonadFix Moment where
-    mfix f = M $ mfix (unM . f)
-
--- Forget all event occurences before a particular time
-forgetE :: Time -> Event a -> [Maybe a]
-forgetE time (E xs) = drop time xs
-
-stepper :: a -> Event a -> Moment (Behavior a)
-stepper i e = M $ \time -> B $ replicate time i ++ step i (forgetE time e)
-    where
-    step i ~(x:xs) = i : step next xs
-        where next = case x of
-                        Just i  -> i
-                        Nothing -> i
-
--- Expressed using recursion and the other primitives
--- FIXME: Strictness!
 accumE :: a -> Event (a -> a) -> Moment (Event a)
-accumE a e1 = mdo
-    let e2 = ((\a f -> f a) <$> b) `apply` e1
-    b <- stepper a e2
-    return e2
-
-{-----------------------------------------------------------------------------
-    Higher-order
-------------------------------------------------------------------------------}
-valueB :: Behavior a -> Moment a
-valueB (B b) = M $ \time -> b !! time
+accumE x fs = mdo
+  let gs = fmap (&) ys `apply` fs
+  ys <- stepper x gs
+  pure gs
 
 observeE :: Event (Moment a) -> Event a
-observeE = E . zipWith (\time -> fmap (\m -> unM m time)) [0..] . unE
+observeE = apply (fmap (&) time)
+
+forgetDiagonalE :: Event (Event a) -> Event (Event a)
+forgetDiagonalE = liftA2 forgetE timeE
 
 switchE :: Event (Event a) -> Moment (Event a)
-switchE es = M $ \t -> E $
-    replicate t Nothing ++ switch (unE never) (forgetE t (forgetDiagonalE es))
+switchE xss n = Compose $ take n (unE never) `prependList` switch (unE never) (drop n (unE (forgetDiagonalE xss)))
     where
-    switch (x:xs) (Nothing : ys) = x : switch xs ys
-    switch (x: _) (Just xs : ys) = x : switch (tail xs) ys
+    switch (Behavior (x, _)) (Behavior (Just (Compose xs), ys)) = x `cons` switch (tail xs) ys
+    switch (Behavior (x, xs)) (Behavior (_, ys)) = x `cons` switch xs ys
+    -- switch xs ys = case head ys of
+       -- Just xs' -> cons (head xs) (switch (tail xs') ys)
+       -- _ -> cons (head xs) (switch (tail xs) ys)
 
-forgetDiagonalE :: Event (Event a) -> Event [Maybe a]
-forgetDiagonalE = E . zipWith (\time -> fmap (forgetE time)) [0..] . unE
+--- Functions that combine Behaviors and Events ---
+
+-- | Apply a Behavior function to an Event to get a new Event.
+apply :: Behavior (a -> b) -> Event a -> Event b
+apply fs = Compose . liftA2 fmap fs . getCompose
+
+stepper :: a -> Event a -> Moment (Behavior a)
+stepper x (Compose xs) n = prependList
+  (replicate (fromIntegral (getMax n)) x)
+  (step x (drop n xs))
+  where
+  step y ys = cons y (step (fromMaybe y (head ys)) (tail ys))
+
+behaviorToEvent :: Behavior a -> Event a
+behaviorToEvent = Compose . fmap Just
 
 switchB :: Behavior a -> Event (Behavior a) -> Moment (Behavior a)
-switchB b e = diagonalB <$> stepper b e
-
-diagonalB :: Behavior (Behavior a) -> Behavior a
-diagonalB = B . zipWith (\time xs -> xs !! time) [0..] . map unB . unB
+switchB xs xss = diagonalB <$> stepper xs xss
