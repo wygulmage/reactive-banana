8d5052c3a6b26cb0f6082e117e414d89ecb5520f
Keith <wygulmage@users.noreply.github.com>
2019-08-13 22:07:37 -0400

    changed time type to (Sum Natural).

--- reactive-banana/src/Reactive/Banana/Type/Behavior.hs
+++ reactive-banana/src/Reactive/Banana/Type/Behavior.hs
@@ -4,13 +4,14 @@
 
 module Reactive.Banana.Type.Behavior where
 
-import Prelude ((+), (-), fst, snd)
+import Prelude ((+), (-), flip, fst, snd)
 
 import Control.Applicative
 import Control.Category ((.), id)
 import Data.Functor
 import Data.Bifunctor (bimap)
 import Data.Semigroup
+import Data.Monoid
 import Data.Foldable
 import Numeric.Natural
 
@@ -18,6 +19,7 @@ import Numeric.Natural
 -- A behavior is modeled as an /infinite/ list of values.
 newtype Behavior a = Behavior{ getBehavior ::(a, Behavior a) }
 
+type Moment = (->) (Sum Natural)
 
 ----- Instances -----
 
@@ -43,12 +45,19 @@ instance Foldable Behavior where
 
 ----- Functions -----
 
+-- | Convert a Behavior to a Moment.
+-- (!!) :: Behavior a -> (Natural -> a)
+(!!) :: Behavior a -> Moment a
+xs !! 0 = head xs
+xs !! i = tail xs !! (i - 1)
+
+-- | Convert a Moment to a Behavior
+memo :: Moment a -> Behavior a
+memo f = go 0
+   where go i = Behavior (f i, go (i + 1))
+
 diagonalB :: Behavior (Behavior a) -> Behavior a
-diagonalB = liftA2 indexing (behaviorFrom (1 +) 0)
-  where
-  indexing :: Natural -> Behavior a -> a
-  indexing 0 = head
-  indexing i = indexing (i - 1) . tail
+diagonalB = liftA2 (flip (!!)) (behaviorFrom (1 +) 0)
 
 behaviorFrom :: (a -> a) -> a -> Behavior a
 behaviorFrom f = go
@@ -67,10 +76,13 @@ tail = snd . getBehavior
 tails :: Behavior a -> Behavior (Behavior a)
 tails = behaviorFrom tail
 
-drop :: Natural -> Behavior a -> Behavior a
+drop :: Sum Natural -> Behavior a -> Behavior a
 drop 0 = id
 drop n = drop (n - 1) . tail
 
-take :: Natural -> Behavior a -> [a]
+take :: Sum Natural -> Behavior a -> [a]
 take 0 _ = []
 take n (Behavior (x, xs)) = x : take (n - 1) xs
+
+prependList :: [a] -> Behavior a -> Behavior a
+prependList = flip (foldr cons)
--- reactive-banana/src/Reactive/Banana/Type/Event.hs
+++ reactive-banana/src/Reactive/Banana/Type/Event.hs
@@ -1,29 +1,41 @@
 
 module Reactive.Banana.Type.Event where
 
-import Reactive.Banana.Type.Behavior
+import qualified Reactive.Banana.Type.Behavior as B
 import Control.Applicative
 import Control.Monad
 import Data.Functor.Compose (Compose (Compose), getCompose)
 import Data.Bifunctor (bimap)
+import Data.Semigroup
+import Data.Maybe (fromMaybe)
+import Numeric.Natural
 
 -- newtype Event a = E{ getEvent :: Behavior (Maybe a) }
-type Event = Compose Behavior Maybe
+type Event = Compose B.Behavior Maybe
 
+unE :: Event a -> B.Behavior (Maybe a)
+unE = getCompose
 
 never :: Event a
 never = Compose (pure Nothing)
 
-unionWith :: (a -> a -> a) -> Event a -> Event a -> Event a
-unionWith = liftA2
 
--- fromBehavior :: Behavior a -> Event a
--- fromBehavior = Compose . Behavior . bimap Just fromBehavior . getBehavior
--- fromBehavior (Behavior (x, xs)) = Compose (Behavior (Just x, fromBehavior xs))
+fromBehavior :: B.Behavior a -> Event a
+fromBehavior = Compose . go
+  where go = B.Behavior . bimap Just go . B.getBehavior
 
 
 filterJust :: Event (Maybe a) -> Event a
 filterJust = Compose . fmap join . getCompose
 
-apply :: Behavior (a -> b) -> Event a -> Event b
+apply :: B.Behavior (a -> b) -> Event a -> Event b
 apply fs = Compose . (<*>) (fmap fmap fs) . getCompose
+
+-- | Just the elements of a list, then Nothing.
+fromList :: [a] -> Event a
+fromList = Compose . go
+  where
+  go = foldr (B.cons . Just) (getCompose never)
+
+forget :: Sum Natural -> Event a -> Event a
+forget n = Compose . B.drop n . getCompose
